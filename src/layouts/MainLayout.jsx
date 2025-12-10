f2d92577914bc58a4cf26ccdc354179719ab9d77
Alamin Islam <alamin648890@gmail.com>
2025-12-10 15:27:21 +0600

    refactor: update layout components
    - Enhanced MainLayout with proper Outlet rendering
    - Improved DashboardLayout with sidebar navigation
    - Fixed flex-grow typo in MainLayout

--- src/layouts/MainLayout.jsx
+++ src/layouts/MainLayout.jsx
@@ -1,16 +1,15 @@
-import React from 'react';
 import { Outlet } from 'react-router-dom';
+import Navbar from '../components/shared/Navbar';
 import Footer from '../components/shared/Footer';
-import NavBar from '../components/shared/NavBar';
 
 const MainLayout = () => {
     return (
-        <div className='flex flex-col min-h-screen'>
-            <NavBar/>
-            <main className='flex-glow'>
-                <Outlet/>
+        <div className="flex flex-col min-h-screen">
+            <Navbar />
+            <main className="flex-grow">
+                <Outlet />
             </main>
-            <Footer/>
+            <Footer />
         </div>
     );
 };
